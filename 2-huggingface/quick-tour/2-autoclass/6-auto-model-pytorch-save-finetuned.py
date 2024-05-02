3aa282b31eab68afe3531ac07ff866026f679297
Manu <Manuel.Fernandez.Yanez@ibm.com>
2024-05-02 09:44:33 +0200

    added huggingface save fine-tuned models for pytorch and tensorflow

---
+++ 2-huggingface/quick-tour/2-autoclass/6-auto-model-pytorch-save-finetuned.py
@@ -0,0 +1,34 @@
+from transformers import AutoModelForSequenceClassification
+from transformers import AutoTokenizer
+from torch import nn
+
+model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
+pt_model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+# Now pass your preprocessed batch of inputs directly to the model. You just have to unpack the dictionary by adding **:
+
+pt_batch = tokenizer(
+    ["We are very happy to show you the 🤗 Transformers library.", "We hope you don't hate it."],
+    padding=True,
+    truncation=True,
+    max_length=512,
+    return_tensors="pt",
+)
+
+pt_outputs = pt_model(**pt_batch)
+
+# The model outputs the final activations in the logits attribute. Apply the softmax function to the logits to retrieve the probabilities:
+
+pt_predictions = nn.functional.softmax(pt_outputs.logits, dim=-1)
+print(pt_predictions)
+
+# Save fine-tuned data
+
+pt_save_directory = "./pt_save_pretrained"
+tokenizer.save_pretrained(pt_save_directory)
+pt_model.save_pretrained(pt_save_directory)
+
+# Reload fine-tuned data
+
+pt_model = AutoModelForSequenceClassification.from_pretrained("./pt_save_pretrained")
---
+++ 2-huggingface/quick-tour/2-autoclass/7-auto-model-tensorflow-save-finetuned.py
@@ -0,0 +1,40 @@
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+from transformers import TFAutoModelForSequenceClassification
+import tensorflow as tf
+
+model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
+tf_model = TFAutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+# Now pass your preprocessed batch of inputs directly to the model. You just have to unpack the dictionary by adding **:
+
+tf_batch = tokenizer(
+    ["We are very happy to show you the 🤗 Transformers library.", "We hope you don't hate it."],
+    padding=True,
+    truncation=True,
+    max_length=512,
+    return_tensors="tf",
+)
+
+# Save fine-tuned data to disk
+
+tf_save_directory = "./tf_save_pretrained"
+tokenizer.save_pretrained(tf_save_directory)
+tf_model.save_pretrained(tf_save_directory)
+
+# Reload fine-tuned data from disk
+
+tf_model = TFAutoModelForSequenceClassification.from_pretrained("./tf_save_pretrained")
+
+
+
+
+tf_outputs = tf_model(tf_batch)
+
+# The model outputs the final activations in the logits attribute. Apply the softmax function to the logits to retrieve the probabilities:
+
+# huggingface says this, but it should bt tf.nn tf_predictions = tf.nn.softmax(tf_outputs.logits, dim=-1)
+
+tf_predictions = tf.nn.softmax(tf_outputs.logits, axis=-1)
+print(tf_predictions)
