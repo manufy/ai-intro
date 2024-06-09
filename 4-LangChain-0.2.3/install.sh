8c7029dc785d45394e28ff41c116b05e6b505dd7
Manu <dev@mfy.es>
2024-06-09 10:48:57 +0200

    added langchain cohere anthropic and ollama

---
+++ 4-LangChain-0.2.3/install.sh
@@ -0,0 +1,8 @@
+pip install langchain
+pip install langchain-core
+pip install langchain-community
+pip install langchain-experimental
+pip install langgraph
+pip install "langserve[all]"
+pip install langchain-cli
+pip install langsmith
