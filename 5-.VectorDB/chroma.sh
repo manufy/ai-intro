994520cbbf52f44c7000fab66fb58e6750451af6
Manu <dev@mfy.es>
2024-06-09 11:53:12 +0200

    chroma db added

--- 5-.VectorDB/chroma.sh
+++
@@ -1,1 +0,0 @@
-git clone git@github.com:chroma-core/chroma.git
---
+++ 5-VectorDB/clone_chroma.sh
@@ -0,0 +1,6 @@
+git clone git@github.com:chroma-core/chroma.git
+
+
+#  add ALLOW_RESET=TRUE to docker-compose environment
+
+# docker-compose up -d --build
